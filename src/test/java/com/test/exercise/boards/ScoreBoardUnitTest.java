23002785a95861763f5cdc89555e1c2683036d74
Simon Yao <simon_yao@epam.com>
2023-07-25 17:28:37 +0800

    1. add scordPrint game function

--- src/test/java/com/test/exercise/boards/ScoreBoardUnitTest.java
+++ src/test/java/com/test/exercise/boards/ScoreBoardUnitTest.java
@@ -1,13 +1,95 @@
 package com.test.exercise.boards;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 
 public class ScoreBoardUnitTest {
 
-	ScoreBoard scoreBoard;
+	private ScoreBoardPrint sBoardPrint;
 	
 	@Test
-	void testScoreCardPrintedInTheBoard() {
-		scoreBoard.printScore();
+	public void testScoreCardPrintedInTheBoard_zero_to_zero() {
+		int p1 = 0;
+		int p2 = 0;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "0:0";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_one_to_zero() {
+		int p1 = 1;
+		int p2 = 0;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "15:0";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_one_to_one() {
+		int p1 = 1;
+		int p2 = 1;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "15:15";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_one_to_two() {
+		int p1 = 1;
+		int p2 = 2;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "15:30";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_one_to_three() {
+		int p1 = 1;
+		int p2 = 3;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "15:40";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_three_to_two() {
+		int p1 = 3;
+		int p2 = 2;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "40:30";
+		assertEquals(expaction,result);
+	}
+	
+	@Test
+	public void testScoreCardPrintedInTheBoard_three_to_three() {
+		int p1 = 3;
+		int p2 = 3;
+		sBoardPrint = new ScoreBoardPrint();
+		sBoardPrint.setP1(p1);
+		sBoardPrint.setP2(p2);
+		String result = sBoardPrint.printScore();
+		String expaction = "DEUCE";
+		assertEquals(expaction,result);
 	}
 }
